7e61cf792c00ee8a71fa86835fbee84d25e4ad28
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2015-02-12 16:09:21 +0100

    mise a jour du run_test

--- run_test.sh
+++ run_test.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-if [ $# -eq 0 ] || [ $1 = '-c' -a $# -eq 1 ]
+if [ $# -eq 0 ] || [ $1 = '-c' -a $# -eq 1 ] || [ $1 = '-a' -a $# -eq 1 ]
 then
-	echo "CORRECT\n"
 	list=`ls "test/correct/"`
 	for file in $list
 	do
-		echo "\033[31mFILE "$file"\033[0m\n"
+		echo "\033[32mFILE "$file"\033[0m\n"
 		./run.sh < "test/correct/"$file
 	done
 
-elif [ $1 = '-i' -a $# -eq 1 ]
+fi
+if [ $# -eq 1 ] && [ $# -eq 1 -a $1 = '-i' ] || [ $# -eq 1 -a $1 = '-a' ]
 then
-	echo "INCORRECT\n"
 	list=`ls "test/incorrect/"`
 	for file in $list
 	do
