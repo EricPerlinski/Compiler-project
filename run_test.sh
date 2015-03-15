adbfbea8a6270b5f6e012c95daf3416bb8fd9299
Jean-Philippe Eisenbarth <max@nurupoga.org>
2015-03-15 22:34:28 +0100

    Même changement que commit précédent.
    Petit soucis : le chemin relatif vers les fichiers .dot n'est pas bon

--- run_test.sh
+++ run_test.sh
@@ -5,24 +5,24 @@ mkdir -p tree/input
 if [ $# -eq 0 ] || [ $1 = '-c' -a $# -eq 1 ] || [ $1 = '-a' -a $# -eq 1 ]
 then
 	list=`ls "test/correct/"`
-	cd bin
+#	cd bin
 	for file in $list
 	do
 		echo "\n\033[32mFILE "$file"\033[0m"
-		java -classpath ".:../antlr.jar" Test "../test/correct/"$file # > "../tree/input/"$file.tree
+		java -classpath ".:antlr.jar:bin" Test "./test/correct/"$file # > "./tree/input/"$file.tree
 	done
-	cd ..
+#	cd ..
 fi
 if [ $# -eq 1 ] && [ $# -eq 1 -a $1 = '-i' ] || [ $# -eq 1 -a $1 = '-a' ]
 then
 	list=`ls "test/incorrect/"`
-	cd bin
+#	cd bin
 	for file in $list
 	do
 		echo "\n\033[31mFILE "$file"\033[0m"
-		java -classpath ".:../antlr.jar" Test  "../test/incorrect/"$file # > "../tree/input/"$file.tree
+		java -classpath ".:antlr.jar:bin" Test  "./test/incorrect/"$file # > "./tree/input/"$file.tree
 	done
-	cd ..
+#	cd ..
 fi
 
 break;
