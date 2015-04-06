c69fb4576a18f7d41b6f72d11a18408af60d99eb
Jean-Philippe Eisenbarth <max@nurupoga.org>
2015-04-06 21:33:07 +0200

    Remove duplicated files and update compileJ script

--- compileJ.sh
+++ compileJ.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd src
-javac *.java plic/*.java model/*.java parser_tools/*.java dot/*.java -d ../bin -cp "../antlr.jar"
+javac plic/*.java model/*.java parser_tools/*.java dot/*.java -d ../bin -cp "../antlr.jar"
